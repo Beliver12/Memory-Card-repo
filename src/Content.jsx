50f657606c727815151340c21ab561ca00500f08
Nikola <nzencic@gmail.com>
2024-03-09 15:45:18 +0100

    pull API of each pokemon and display it

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,10 +1,37 @@
 import { useState, useEffect } from "react";
 
 import './index.css'
-
+import './content.css'
 export default function Content() {
-  const [listOfUrl, setListOfUrl] = useState([])
+ const [listOfSrc, setListOfSrc] = useState([])
   const [lisOfname, setListOfName] = useState([])
+
+  const [score, setScore] = useState(0)
+  const [bestScore, setBestScore] = useState(0)
+
+  const [listOfIds, setListOfIds] = useState([])
+  const currentImgId = [];
+
+  const shuffle = (arr) => {
+    return arr.sort(() => Math.random() - 0.5);
+   }
+
+  function handleClick() {
+   shuffle(listOfSrc)
+   
+    setScore(score + 1)
+    setListOfIds([...listOfIds, {id: currentImgId[0]}])
+    if(listOfIds.length > 0) {
+      for(let i = 0; i < listOfIds.length; i++){
+   if(listOfIds[i].id.includes(currentImgId[0])) {
+    setBestScore(score)
+    setScore(0)
+    setListOfIds([])
+   } 
+  } } 
+  setListOfSrc(listOfSrc)
+}
+   
 useEffect(() => {
   fetch('https://pokeapi.co/api/v2/pokemon/')
 .then(function (response) {
@@ -16,39 +43,48 @@ useEffect(() => {
 })
 },[]);
 
-useEffect(() => {
-{lisOfname.map(list => 
+
+const listOfUrl = lisOfname.map(list => 
   fetch(list.url)
   .then(function (response) {
      return response.json();
    }).then(function (response) {
-    //console.log(response)
-    listOfUrl.push(response.sprites.back_default)
-     
-console.log(listOfUrl)
+
+    return response
+    
    })
- )}
-},[lisOfname, listOfUrl]);
-
-
-
-
-
-
+ )
+ useEffect(() => {
+ Promise.all(listOfUrl).then(function(res) { 
+  setListOfSrc(res)
+})
+},[listOfUrl]);
 
   return (
-      <div>
-          <center>
-             <div>
-              {listOfUrl.map((list, i) => 
-              <img src={list} key={i} alt="" />
-              )}
-             </div>
-       
-              {lisOfname.map(name => 
-                  <p key={name.name}>{name.name}</p>
-                  )}
-          </center>
+    <div>
+      <div className='Header'>
+        <h1>Memory-Card-Game</h1>
+        <div className='ScoreBoard'>
+          <h3>Score:{score}</h3>
+          <h3>Best-Score:{bestScore}</h3>
+        </div>
       </div>
+            <center className="img-container">
+      
+                 {listOfSrc.map(list =>
+                 <div key={list.id} 
+                 onClick={handleClick}>
+                  <img 
+                  onClick={(e) => currentImgId.push(e.target.id)} 
+                  src={list.sprites.back_shiny} id={list.id}
+                   key={list.id} alt="" />
+                  {list.name}
+                  
+                  </div>
+                  )}
+      
+            </center>
+    </div>
+     
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Header from './Header.jsx'
+
 import './index.css'
 import Content from './Content.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Header />
     <Content />
   </React.StrictMode>,
 )
