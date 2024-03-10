6a649b10d2a1fe1d36ddf310e92b19cf0e1b5f93
Nikola <nzencic@gmail.com>
2024-03-10 14:51:07 +0100

    finished

--- src/Content.jsx
+++ src/Content.jsx
@@ -16,21 +16,7 @@ export default function Content() {
     return arr.sort(() => Math.random() - 0.5);
    }
 
-  function handleClick() {
-   shuffle(listOfSrc)
-   
-    setScore(score + 1)
-    setListOfIds([...listOfIds, {id: currentImgId[0]}])
-    if(listOfIds.length > 0) {
-      for(let i = 0; i < listOfIds.length; i++){
-   if(listOfIds[i].id.includes(currentImgId[0])) {
-    setBestScore(score)
-    setScore(0)
-    setListOfIds([])
-   } 
-  } } 
-  setListOfSrc(listOfSrc)
-}
+
    
 useEffect(() => {
   fetch('https://pokeapi.co/api/v2/pokemon/')
@@ -56,14 +42,37 @@ const listOfUrl = lisOfname.map(list =>
  )
  useEffect(() => {
  Promise.all(listOfUrl).then(function(res) { 
+  if(listOfSrc.length < 1) {
   setListOfSrc(res)
+  }
 })
 },[listOfUrl]);
+
+function handleClick() {
+  const shuffledList = [...listOfSrc]
+  shuffle(shuffledList)
+  setListOfSrc(shuffledList)
+  setScore(score + 1)
+  setListOfIds([...listOfIds, {id: currentImgId[0]}])
+  if(listOfIds.length > 0) {
+    for(let i = 0; i < listOfIds.length; i++){
+ if(listOfIds[i].id.includes(currentImgId[0]) && score > bestScore) {
+  setBestScore(score)
+  setScore(0)
+  setListOfIds([])
+ } else if(listOfIds[i].id.includes(currentImgId[0]) && score < bestScore) {
+  setScore(0)
+  setListOfIds([])
+ }
+} } 
+
+}
 
   return (
     <div>
       <div className='Header'>
         <h1>Memory-Card-Game</h1>
+        <h3>Get points by clicking on an image but don&apos;t click on any more than once!</h3>
         <div className='ScoreBoard'>
           <h3>Score:{score}</h3>
           <h3>Best-Score:{bestScore}</h3>
@@ -72,7 +81,7 @@ const listOfUrl = lisOfname.map(list =>
             <center className="img-container">
       
                  {listOfSrc.map(list =>
-                 <div key={list.id} 
+                 <div key={list.id} className="img-wrapper"
                  onClick={handleClick}>
                   <img 
                   onClick={(e) => currentImgId.push(e.target.id)} 
